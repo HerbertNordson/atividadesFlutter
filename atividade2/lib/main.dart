9134c3c3fb6b8692713380bb7de61050cfdb2733
Herbert Nordson <herbert_nordson@hotmail.com>
2021-04-07 09:53:25 -0300

    Finalização da Atividade.

--- atividade2/lib/main.dart
+++ atividade2/lib/main.dart
@@ -9,11 +9,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Container(
-       
-     ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.blue,
+            child: Row(
+              children: [
+                Container(
+                  child: Column(children: [
+                    Container(
+                      color: Colors.green,
+                      height: 200,
+                      width: 192,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                      height: 200,
+                      width: 192,
+                    ),
+                  ]),
+                ),
+                Container(
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                      color: Colors.brown,
+                      height: 300,
+                      width: 100,
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                      color: Colors.yellowAccent,
+                      height: 300,
+                      width: 100,
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              color: Colors.pink,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    color: Colors.purple,
+                    width: 100,
+                  )
+                ],
+              )),
+          Container(
+            color: Colors.blue,
+            height: 100,
+          ),
+        ],
+      ),
     );
   }
 }
-
-
